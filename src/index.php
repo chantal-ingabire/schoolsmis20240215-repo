6b967ba86f6ec1464b48a3c289026a0f7f1e6ce8
chantal <chantalinbabire44@gmail.com>
2024-02-15 15:23:46 +0200

    change in index file
    file index chaged

--- src/index.php
+++ src/index.php
@@ -1,2 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=h, initial-scale=1.0">
+    <title>School MIS</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
 <?php
-echo "<h3> we are dealing with PHP</h3>";
+echo "<h3> we are dealing with PHP</h3>";?>
+
+</body>
+</html>
